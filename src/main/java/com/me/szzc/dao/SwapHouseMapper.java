607ef4f2b569dafd26646349c4116f36ad931c92
hateluwei <hateluwei@126.com>
2019-08-02 10:25:10 +0800

    点房

--- src/main/java/com/me/szzc/dao/SwapHouseMapper.java
+++ src/main/java/com/me/szzc/dao/SwapHouseMapper.java
@@ -1,7 +1,10 @@
 package com.me.szzc.dao;
 
+import com.me.szzc.pojo.dto.ChooseHouseDTO;
 import com.me.szzc.pojo.entity.SwapHouse;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface SwapHouseMapper {
     int deleteByPrimaryKey(Long id);
@@ -19,5 +22,8 @@ public interface SwapHouseMapper {
     SwapHouse getByHouseOwnerAddr(@Param("houseOwner") String houseOwner, @Param("address") String address);
 
     int  delete(SwapHouse swapHouse);
+
+    /**通过结算单id获取选房信息**/
+    List<ChooseHouseDTO> getChooseHouseListBySettleId(@Param("settleId") Long settleId);
 
 }
