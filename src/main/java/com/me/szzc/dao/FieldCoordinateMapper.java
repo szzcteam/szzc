d0a050c721d5397cf6c9fd6696ed84628b1f2eb0
bbfang <bbfang@iflytek.com>
2019-08-12 18:11:30 +0800

    结算单打印

---
+++ src/main/java/com/me/szzc/dao/FieldCoordinateMapper.java
@@ -0,0 +1,15 @@
+package com.me.szzc.dao;
+
+import com.me.szzc.pojo.dto.FieldCoordinateDto;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * 打印字段坐标Mapper
+ * Created by bbfang on 2019/8/12.
+ */
+public interface FieldCoordinateMapper {
+
+    List<FieldCoordinateDto> getFieldCoordinateListByTableName(@Param("tableName")String tableNamesss);
+}
