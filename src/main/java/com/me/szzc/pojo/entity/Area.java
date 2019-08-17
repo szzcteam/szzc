f906bc1fcc2f915597247f840cb22d60d7fb76dc
hateluwei <hateluwei@126.com>
2019-08-17 13:43:06 +0800

    增加mybatis自动生成支持

--- src/main/java/com/me/szzc/pojo/entity/Area.java
+++ src/main/java/com/me/szzc/pojo/entity/Area.java
@@ -1,22 +1,87 @@
 package com.me.szzc.pojo.entity;
 
-import lombok.Data;
-
-import java.util.List;
-
-@Data
 public class Area {
-
-    /***主键id**/
+    /**
+     * 
+     */
     private Long id;
 
-    /**名称**/
+    /**
+     * 片区名称
+     */
     private String name;
 
-    /**状态：0启用、1禁用**/
+    /**
+     * 状态：0启用、1禁用
+     */
     private Integer status;
 
-    /**逻辑删除标识**/
-    private Boolean deleted;
+    /**
+     * 逻辑删除标识
+     */
+    private Byte deleted;
 
+    /**
+     * 
+     * @return id 
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * 
+     * @param id 
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * 片区名称
+     * @return name 片区名称
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 片区名称
+     * @param name 片区名称
+     */
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    /**
+     * 状态：0启用、1禁用
+     * @return status 状态：0启用、1禁用
+     */
+    public Integer getStatus() {
+        return status;
+    }
+
+    /**
+     * 状态：0启用、1禁用
+     * @param status 状态：0启用、1禁用
+     */
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    /**
+     * 逻辑删除标识
+     * @return deleted 逻辑删除标识
+     */
+    public Byte getDeleted() {
+        return deleted;
+    }
+
+    /**
+     * 逻辑删除标识
+     * @param deleted 逻辑删除标识
+     */
+    public void setDeleted(Byte deleted) {
+        this.deleted = deleted;
+    }
 }
