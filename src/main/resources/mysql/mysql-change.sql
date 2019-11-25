5b6ed0d8cb447a030b68df0abbe3ef6835f8e1d0
bbfang <bbfang@iflytek.com>
2019-11-25 10:17:55 +0800

    打印坐标表维护sql脚本

--- src/main/resources/mysql/mysql-change.sql
+++ src/main/resources/mysql/mysql-change.sql
@@ -13,3 +13,9 @@ ADD COLUMN `project_code`  varchar(50) NULL COMMENT '管辖项目';
 
 #设置当前片区为明伦街
 update t_area set project_code = 'A001001';
+
+#2019/11/25 打印坐标表增加字段项目编码,并维护明伦街项目编码。
+ALTER TABLE `field_coordinate`
+    ADD COLUMN `item_code` VARCHAR(32) NOT NULL COMMENT '项目编码' AFTER `del`;
+
+UPDATE field_coordinate SET item_code = 'A001001';
