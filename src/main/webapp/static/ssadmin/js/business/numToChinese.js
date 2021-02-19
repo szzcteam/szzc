fa907828713f8b153fa42abeaa636214b98ab94a
luwei <luwei@sapling.com.cn>
2021-02-19 16:14:28 +0800

    前端升级:数字转金额

--- src/main/webapp/static/ssadmin/js/business/numToChinese.js
+++ src/main/webapp/static/ssadmin/js/business/numToChinese.js
@@ -1,121 +1,190 @@
-//阿拉伯数字转换为简写汉字 900 -> 九百
-function Arabia_To_SimplifiedChinese(Num) {
-    for (i = Num.length - 1; i >= 0; i--) {
-        Num = Num.replace(",", "")//替换Num中的“,”
-        Num = Num.replace(" ", "")//替换Num中的空格
-    }
-    if (isNaN(Num)) { //验证输入的字符是否为数字
-        //alert("请检查小写金额是否正确");
-        return;
-    }
-    //字符处理完毕后开始转换，采用前后两部分分别转换
-    part = String(Num).split(".");
-    newchar = "";
-    //小数点前进行转化
-    for (i = part[0].length - 1; i >= 0; i--) {
-        if (part[0].length > 10) {
-            //alert("位数过大，无法计算");
-            return "";
-        }//若数量超过拾亿单位，提示
-        tmpnewchar = ""
-        perchar = part[0].charAt(i);
-        switch (perchar) {
-            case "0":  tmpnewchar = "零" + tmpnewchar;break;
-            case "1": tmpnewchar = "一" + tmpnewchar; break;
-            case "2": tmpnewchar = "二" + tmpnewchar; break;
-            case "3": tmpnewchar = "三" + tmpnewchar; break;
-            case "4": tmpnewchar = "四" + tmpnewchar; break;
-            case "5": tmpnewchar = "五" + tmpnewchar; break;
-            case "6": tmpnewchar = "六" + tmpnewchar; break;
-            case "7": tmpnewchar = "七" + tmpnewchar; break;
-            case "8": tmpnewchar = "八" + tmpnewchar; break;
-            case "9": tmpnewchar = "九" + tmpnewchar; break;
-        }
-        switch (part[0].length - i - 1) {
-            case 0: tmpnewchar = tmpnewchar; break;
-            case 1: if (perchar != 0) tmpnewchar = tmpnewchar + "十"; break;
-            case 2: if (perchar != 0) tmpnewchar = tmpnewchar + "百"; break;
-            case 3: if (perchar != 0) tmpnewchar = tmpnewchar + "千"; break;
-            case 4: tmpnewchar = tmpnewchar + "万"; break;
-            case 5: if (perchar != 0) tmpnewchar = tmpnewchar + "十"; break;
-            case 6: if (perchar != 0) tmpnewchar = tmpnewchar + "百"; break;
-            case 7: if (perchar != 0) tmpnewchar = tmpnewchar + "千"; break;
-            case 8: tmpnewchar = tmpnewchar + "亿"; break;
-            case 9: tmpnewchar = tmpnewchar + "十"; break;
-        }
-        newchar = tmpnewchar + newchar;
-    }
-    //替换所有无用汉字，直到没有此类无用的数字为止
-    while (newchar.search("零零") != -1 || newchar.search("零亿") != -1 || newchar.search("亿万") != -1 || newchar.search("零万") != -1) {
-        newchar = newchar.replace("零亿", "亿");
-        newchar = newchar.replace("亿万", "亿");
-        newchar = newchar.replace("零万", "万");
-        newchar = newchar.replace("零零", "零");
-    }
-    //替换以“一十”开头的，为“十”
-    if (newchar.indexOf("一十") == 0) {
-        newchar = newchar.substr(1);
-    }
-    //替换以“零”结尾的，为“”
-    if (newchar.lastIndexOf("零") == newchar.length - 1) {
-        newchar = newchar.substr(0, newchar.length - 1);
-    }
-    return newchar;
-}
-
-
 //阿拉伯数字转换为中文汉字 900 -> 玖佰
-function Araia_To_Chinese(num){
-    var fraction = ["角", "分"];
-    var digit = ["零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"];
-    var unit = [["元", "万", "亿"],["", "拾","佰", "仟"]];
-
-    if (isNaN(num)) { //验证输入的字符是否为数字
-        //alert("请检查小写金额是否正确");
-        return;
-    }
-
-    var heads = num < 0 ? "负" : "";
-    //取绝对值
-    num = Math.abs(num);
-    var s = "";
-    for(var i=0; i<fraction.length;i++){
-        s+=(digit[Math.floor(num*10*Math.pow(10, i))%10]+fraction[i]).replace(/(零.)+/,"");
-    }
-    s = s || '整';
-    var integerPart =  Math.floor(num);
-    for (var i = 0; i < unit[0].length && integerPart > 0; i++) {
-        var p = "";
-        for (var j = 0; j < unit[1].length && num > 0; j++) {
-            p = digit[integerPart % 10] + unit[1][j] + p;
-            integerPart = parseInt(integerPart / 10);
-        }
-        s = p.replace(/(零.)*零$/, "").replace(/^$/, "零") + unit[0][i] + s;
-    }
-    //循序替换
-    var returnStr = heads + s.replace(/(零.)*零元/, '元');
-    // console.log("1="+returnStr);
-    //处理首个零
-    returnStr = replaceFirstZero(returnStr);
-    // console.log("2="+returnStr);
-    returnStr = returnStr.replace(/(零.)+/, "零");
-    // console.log("3="+returnStr);
-    returnStr = returnStr.replace(/^整$/, "零元整");
-    // console.log("4="+returnStr);
-    // console.log(returnStr);
-    return returnStr;
-
-
+function Araia_To_Chinese(num) {
+    var amount = numberFormatOptions.format(num, true, true);
+    return amount;
 }
 
-//数字前面补0的，替换为大写的零
-function replaceFirstZero(str) {
-    var tmpStr = str.substr(0,1);
-    // console.log("首个字符是:" + tmpStr);
-    if(tmpStr == "零") {
-        str = str.substr(2, str.length);
-        return replaceFirstZero(str);
-    }else{
-        return str;
+//定义数字格式化对象
+var numberFormatOptions = new Object();
+
+//事先定义好固定数组
+numberFormatOptions.simple_digits = new Array("零", "一", "二", "三", "四", "五", "六", "七", "八", "九");
+numberFormatOptions.traditional_digits = new Array("零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖");
+numberFormatOptions.simple_units = new Array("", "十", "百", "千");
+numberFormatOptions.traditional_units = new Array("", "拾", "佰", "仟");
+
+/**
+ * 阿拉伯数字转换成中文
+ * 小数点后四舍五入保留2位小数
+ * @param amount  阿拉伯数字
+ * @param isUseTraditional   是否使用金额大写
+ * @param isMoneyMode  是否是金额模式
+ */
+numberFormatOptions.format = function (amount, isUseTraditional, isMoneyMode) {
+    var numArray = null;
+    var units = null;
+    if (isUseTraditional) {
+        numArray = numberFormatOptions.traditional_digits;
+    } else {
+        numArray = numberFormatOptions.simple_digits;
     }
+
+    if (amount < -99999999999999.99 || amount > 99999999999999.99) {
+        console.log("不支持的数字 " + amount);
+        return amount;
+    }
+
+    //是负数
+    var negative = false;
+    if (amount < 0.0) {
+        negative = true;
+        amount = -amount;
+    }
+
+    var temp = Math.round(amount * 100);
+    //分
+    var numFen = Math.ceil(temp % 10)
+    temp /= 10;
+    temp = parseInt(temp);
+    //角
+    var numJiao = Math.ceil(temp % 10);
+    temp /= 10;
+    temp = parseInt(temp);
+    var parts = new Array(20);
+    var numParts = 0;
+
+    for (var i = 0; temp != 0; ++i) {
+        var part = Math.ceil(temp % 10000)
+        parts[i] = part;
+        ++numParts;
+        temp /= 10000;
+        temp = parseInt(temp);
+    }
+
+    var beforeWanIsZero = true;
+    var chineseStr = "";
+    for (var i = 0; i < numParts; ++i) {
+        var partChinese = numberFormatOptions.toChinese(parts[i], isUseTraditional);
+        if (i % 2 == 0) {
+            if (partChinese == "") {
+                beforeWanIsZero = true;
+            } else {
+                beforeWanIsZero = false;
+            }
+        }
+
+        if (i != 0) {
+            if (i % 2 == 0) {
+                chineseStr = "亿" + chineseStr;
+            } else if (partChinese == "" && !beforeWanIsZero) {
+                chineseStr = "零" + chineseStr;
+            } else {
+                if (parts[i - 1] < 1000 && parts[i - 1] > 0) {
+                    chineseStr = "零" + chineseStr;
+                }
+
+                if (parts[i] > 0) {
+                    chineseStr = "万" + chineseStr;
+                }
+            }
+        }
+
+        chineseStr = partChinese + chineseStr;
+    }
+
+    //为空，就直接是零
+    if ("" == chineseStr) {
+        chineseStr = numArray[0];
+    }
+
+    if (negative) {
+        chineseStr = "负" + chineseStr;
+    }
+
+    if (numFen == 0 && numJiao == 0) {
+        if (isMoneyMode) {
+            chineseStr = chineseStr + "元整";
+        }
+    } else if (numFen == 0) {
+        if (isMoneyMode) {
+            chineseStr = chineseStr + "元";
+        } else {
+            chineseStr = chineseStr + "点";
+        }
+
+        chineseStr = chineseStr + numArray[numJiao];
+        if (isMoneyMode) {
+            chineseStr = chineseStr + "角";
+        } else {
+            chineseStr = chineseStr + "";
+        }
+    } else if (numJiao == 0) {
+        if (isMoneyMode) {
+            chineseStr = chineseStr + "元零";
+        } else {
+            chineseStr = chineseStr + "点零";
+        }
+        chineseStr = chineseStr + numArray[numFen];
+        if (isMoneyMode) {
+            chineseStr = chineseStr + "分";
+        } else {
+            chineseStr = chineseStr + "";
+        }
+    } else {
+        if (isMoneyMode) {
+            chineseStr = chineseStr + "元";
+        } else {
+            chineseStr = chineseStr + "点";
+        }
+        chineseStr = chineseStr + numArray[numJiao];
+        if (isMoneyMode) {
+            chineseStr = chineseStr + "角";
+        } else {
+            chineseStr = chineseStr + "";
+        }
+        chineseStr = chineseStr + numArray[numFen];
+        if (isMoneyMode) {
+            chineseStr = chineseStr + "分";
+        } else {
+            chineseStr = chineseStr + "";
+        }
+    }
+
+    return chineseStr.toString();
+}
+
+
+//转中文
+numberFormatOptions.toChinese = function (amountPart, isUseTraditional) {
+    var numArray = null;
+    var units = null;
+    if (isUseTraditional) {
+        numArray = numberFormatOptions.traditional_digits;
+        units = numberFormatOptions.traditional_units;
+    } else {
+        numArray = numberFormatOptions.simple_digits;
+        units = numberFormatOptions.simple_units;
+    }
+
+    var temp = amountPart;
+    var chineseStr = "";
+    var lastIsZero = true;
+
+    for (var i = 0; temp > 0; ++i) {
+        var digit = temp % 10;
+        if (digit == 0) {
+            if (!lastIsZero) {
+                chineseStr = "零" + chineseStr;  //第0位 设为 零
+            }
+            lastIsZero = true;
+        } else {
+            lastIsZero = false;
+            chineseStr = numArray[digit] + units[i] + chineseStr;  //第0位设为 具体数字
+        }
+        temp /= 10;
+        temp = parseInt(temp);
+    }
+
+    return chineseStr.toString();
+
 }
